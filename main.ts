dc326c7a82ec6e3d595cffbf01c13b80c8dc0012
Victor Camara Lessa <vlessa.terceiro@brasilcap.com.br>
2019-12-12 18:21:22 -0300

    First commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+import { cardModel, messageModel } from './object-card'
+
+export const notifyCard:Function = (error:Object) => {
+  
+}
+
+export const notifyText:Function = (error:Object) => {
+
+  let bodyMessage = messageModel(error)
+
+  
+
+}
